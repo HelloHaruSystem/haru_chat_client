f7342173a9e4b0bae2e59ffbc6a5994243f27472
Halfdan <halfdansc@gmail.com>
2025-04-25 09:58:23 +0200

    refactored the MessageCell to use the new bubble component

--- src/main/java/com/example/haru/view/MessageCell.java
+++ src/main/java/com/example/haru/view/MessageCell.java
@@ -1,15 +1,10 @@
 package com.example.haru.view;
 
-import java.time.format.DateTimeFormatter;
-
 import com.example.haru.model.ChatMessage;
+import com.example.haru.view.components.MessageBubble;
 
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+
 
 public class MessageCell extends ListCell<ChatMessage> {
     
@@ -21,52 +16,11 @@ public class MessageCell extends ListCell<ChatMessage> {
             setText(null);
             setGraphic(null);
         } else {
-            // create message bubble
-            HBox messageContainer = new HBox(10);
+            // create a new bubble component
+            MessageBubble messageBubble = new MessageBubble(message);
 
-            // style based on the sender
-            //TODO: do this in css later
-            if (message.isFromCurrentUser()) {
-                messageContainer.setAlignment(Pos.CENTER_RIGHT);
-            } else {
-                messageContainer.setAlignment(Pos.BASELINE_LEFT);
-            }
-
-            // create the message bubble itself
-            //TODO: do this in css later
-            VBox bubble = new VBox(5);
-            bubble.getStyleClass().add("message-bubble");
-            bubble.setPadding(new Insets(8, 12, 8, 12));
-            bubble.setMaxWidth(300);
-
-            // add sender name if not from current user
-            //TODO: remove this feature from the server and let client handle it
-            if (!message.isFromCurrentUser()) {
-                Label senderLabel = new Label(message.getSender());
-                senderLabel.setStyle("-fx-font-weight: bold;");
-                bubble.getChildren().add(senderLabel);
-            }
-
-            // add the contents of the message
-            Label contentLabel = new Label(message.getContent());
-            contentLabel.setWrapText(true);
-
-            // add timestamp to the message
-            Label timeLabel = new Label(message.getTimeStamp().format(
-                DateTimeFormatter.ofPattern("HH:mm")));
-            timeLabel.setStyle("-fx-font-size: 10px; -fx-text-fill: #888888");
-
-            bubble.getChildren().addAll(contentLabel, timeLabel);
-
-            // style the bubble
-            if (message.isFromCurrentUser()) {
-                bubble.setStyle("-fx-background-color: #DCF8C6; -fx-background-radius: 10;");
-            } else {
-                bubble.setStyle("-fx-background-color: #FFFFFF; -fx-background-radius: 10;");
-            }
-
-            messageContainer.getChildren().add(bubble);
-            setGraphic(messageContainer);
+            // set the bubble as this cell's graphic
+            setGraphic(messageBubble);
             setText(null);
         }
     }
